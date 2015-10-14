4909bcc5dda826a57e430841e82baaf860eea573
lizthebiz <lizdalay@gmail.com>
2015-10-13 23:06:00 -0700

    Project setup, Intro screen (without options) and started Sign In screen
    ~2 hours of futzing

---
+++ carousel/IntroViewController.swift
@@ -0,0 +1,41 @@
+//
+//  IntroViewController.swift
+//  carousel
+//
+//  Created by Liz Dalay on 10/13/15.
+//  Copyright © 2015 Liz Dalay. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var introScrollView: UIScrollView!
+    
+    @IBOutlet weak var introImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        introScrollView.contentSize = introImageView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
