0d07ba4d9ac819d47af26c7b1d48ca4c89c4461d
lizthebiz <lizdalay@gmail.com>
2015-10-19 15:41:22 -0700

    Attempting optional story of transforming tiles on intro screen

--- carousel/IntroViewController.swift
+++ carousel/IntroViewController.swift
@@ -8,34 +8,46 @@
 
 import UIKit
 
-class IntroViewController: UIViewController {
+class IntroViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var introScrollView: UIScrollView!
     
     @IBOutlet weak var introImageView: UIImageView!
+    
+    @IBOutlet weak var tile1ImageView: UIImageView!
+    
+    @IBOutlet weak var tile2ImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        introScrollView.delegate = self
+        
         introScrollView.contentSize = introImageView.image!.size
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func scrollViewDidScroll(introScrollView: UIScrollView) {
+        // This method is called as the user scrolls
+        
+        tile1ImageView.transform = CGAffineTransformMakeScale(0.25, 0.25)
+        tile1ImageView.transform = CGAffineTransformRotate(view.transform, CGFloat(345 * M_PI / 180))
+    
     }
-    */
+    
+    func scrollViewDidEndDecelerating(introScrollView: UIScrollView) {
+        
+        tile1ImageView.transform = CGAffineTransformMakeScale(1, 1)
+        tile1ImageView.transform = CGAffineTransformRotate(view.transform, CGFloat(0 * M_PI / 180))
+        tile1ImageView.transform = CGAffineTransformMakeTranslation(12, 405)
+    }
 
 }
